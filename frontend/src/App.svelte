29e12fd3b0806c32200c0e87c839bfa2c549ff4c
박찬희 <pch0989@gmail.com>
2022-02-25 15:26:11 +0900

    feat : frontend init
    - svelte 라우터 설정
    - routes, components 폴더 추가
    - prettier 설정 생성

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Router from 'svelte-spa-router';
+  import Login from './routes/Login.svelte';
+  import Main from './routes/Main.svelte';
+
+  const routes = {
+    '/': Main,
+    '/login': Login,
+  };
+</script>
+
+<body>
+  <Router routes={routes} />
+</body>
