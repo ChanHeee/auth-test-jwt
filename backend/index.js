fface06821e385c1781a778890fa880bc9357d0e
박찬희 <pch0989@gmail.com>
2022-02-25 10:18:15 +0900

    feat : backend init
    - 폴더 생성 (routes, middlewares, utils)
    - index.js 라우팅 기능 추가

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const session = require("express-session")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const dotenv = require("dotenv")
+
+const indexRouter = require("./routes/indexRoutes")
+
+dotenv.config()
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+)
+
+app.use("/", indexRouter)
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
+  error.status = 404
+  next(error)
+})
+
+app.use((err, req, res, next) => {
+  res.send(`<h1>${err.message}</h1>
+        <h2>${err.status}</h2>
+        <pre>${err.stack}</pre>`)
+})
+
+app.listen(process.env.NODE_PORT || 5000, () => {
+  console.log(`${process.env.PORT || 5000}번 포트에서 대기 중`)
+})
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Auth app 연습
+
+## JWT를 이용한 로그인 구현
+
+<br>
+
+<!-- > 실행 방법
+
+1. dependencies 설치
+
+   ```bash
+   cd svelte-test
+   npm install
+   ```
+
+2. 빌드 후 실행
+   ```bash
+   npm run dev
+   ``` -->
